aeffbf20c91da0bf6969d3ad9737d77c3b4d088d
unknown <premrnjan@gmail.com>
2023-02-11 17:06:18 +0530

    Try catch added

--- public/app.js
+++ public/app.js
@@ -1,9 +1,29 @@
 // alert("Hello")
-var count = 0;
-performance.getEntries().map(item => item.transferSize).forEach(size => {
-if (typeof size == 'number') {
-count = count + size;
+
+let count = 0;
+
+function cumulative(){
+    performance.getEntries().map(item => item.transferSize).forEach(size => {
+        if (typeof size == 'number') {
+            count = count + size;
+        }
+    });
+    return count;
 }
-// alert("Hello")
-});
-alert(count);
+
+let res = cumulative();
+alert(res);
+try {
+    let data = document.querySelector(".section");
+    window.addEventListener('DOMContentLoaded', ()=> {
+        data.innerHTML = res;
+    })
+    alert(data.textContent.toString);
+    if (data===null) {
+        data.textContent = "hi";
+        throw Error;
+    }
+}
+catch(err) {
+    alert(err);
+}
